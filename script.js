8214a7d5f164ac76ecdbe37f1beb94b6316834fb
gui1949 <guilhermesantiago12@gmail.com>
2021-05-26 22:56:10 -0300

    Brainstorm inicial do sistema baseado no documento enviado e início do desenvolvimento da tela principal.

---
+++ script.js
@@ -0,0 +1,25 @@
+const some_overlay = () => {
+  let cerebro = document.getElementById("cerebro");
+  cerebro.style.animationName = "aparecer";
+
+  setTimeout(() => {
+    document.body.style.animationName = 'bkg_escuro'
+  }, 1000);
+
+  setTimeout(() => {
+    document.getElementById("caminho").style.transformOrigin = ((Math.trunc(cerebro.getBoundingClientRect().left) - 10) + 'px') + ' ' + (Math.trunc(cerebro.getBoundingClientRect().top) + 'px')
+    document.getElementById("caminho").style.transform = "scale(3)";
+    document.getElementById("caminho").style.zIndex = 3;
+    cerebro.style.zIndex = 5;
+
+    setTimeout(() => {
+        document.getElementById("caminho").style.transform = "scale(1)";
+      },5000)
+
+    setTimeout(() => {
+        document.body.style.animationName = 'bkg_voltar'
+    }, 7000);
+
+  },2000)
+
+};
