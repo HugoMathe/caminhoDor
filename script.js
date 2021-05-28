cfb573f46b32006d2a950b486016e9092b38a7ee
gui1949 <guilhermesantiago12@gmail.com>
2021-05-27 22:21:22 -0300

    Overlay OK

--- script.js
+++ script.js
@@ -3,23 +3,26 @@ const some_overlay = () => {
   cerebro.style.animationName = "aparecer";
 
   setTimeout(() => {
-    document.body.style.animationName = 'bkg_escuro'
+    document.body.style.animationName = "bkg_escuro";
   }, 1000);
 
   setTimeout(() => {
-    document.getElementById("caminho").style.transformOrigin = ((Math.trunc(cerebro.getBoundingClientRect().left) - 10) + 'px') + ' ' + (Math.trunc(cerebro.getBoundingClientRect().top) + 'px')
+    document.getElementById("caminho").style.transformOrigin =
+      Math.trunc(cerebro.getBoundingClientRect().left) -
+      10 +
+      "px" +
+      " " +
+      (Math.trunc(cerebro.getBoundingClientRect().top) - 10 + "px");
     document.getElementById("caminho").style.transform = "scale(3)";
     document.getElementById("caminho").style.zIndex = 6;
     cerebro.style.zIndex = 5;
 
     setTimeout(() => {
-        document.getElementById("caminho").style.transform = "scale(1)";
-      },5000)
+      document.getElementById("caminho").style.transform = "scale(1)";
+    }, 5000);
 
     setTimeout(() => {
-        document.body.style.animationName = 'bkg_voltar'
+      document.body.style.animationName = "bkg_voltar";
     }, 7000);
-
-  },2000)
-
+  }, 2000);
 };
